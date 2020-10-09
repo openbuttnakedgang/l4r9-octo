4635dcfc7f1d9de3d80b678c8ba522f98bcbbd7e
fmv <mishka798@mail.ru>
2020-10-09 20:32:51 +0300

    Add ospim enable

--- src/ospi.rs
+++ src/ospi.rs
@@ -39,6 +39,10 @@ impl OSpi {
 
         // Включаю тактирование octospi2
         rcc.ahb3enr.write(|w| w.ospi2en().set_bit());
+        rcc.ahb2enr.write(|w| w.ospimen().set_bit());
+
+        // rcc.ahb2rstr.write(|w| w.ospimrst().set_bit());
+        // rcc.ahb3rstr.write(|w| w.ospi2rst().set_bit());
 
         let mut rcc = p.RCC.constrain();
         let mut gpioi = p.GPIOI.split(&mut rcc.ahb2);
@@ -46,21 +50,18 @@ impl OSpi {
         let mut gpioh = p.GPIOH.split(&mut rcc.ahb2);
 
         // Включение нужных пинов
-        let _clk = gpioi.pi6.into_af5(&mut gpioi.moder, &mut gpioi.afrl).set_speed(Speed::VeryHigh);
-        let _ncs = gpiog.pg12.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::VeryHigh);
-        let _dqs = gpiog.pg15.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::VeryHigh);
+        let _clk = gpioi.pi6.into_af5(&mut gpioi.moder, &mut gpioi.afrl).set_speed(Speed::High);
+        let _ncs = gpiog.pg12.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::High);
+        let _dqs = gpiog.pg15.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::High);
 
-        let _io0 = gpioi.pi11.into_af5(&mut gpioi.moder, &mut gpioi.afrh).set_speed(Speed::VeryHigh);
-        let _io1 = gpioi.pi10.into_af5(&mut gpioi.moder, &mut gpioi.afrh).set_speed(Speed::VeryHigh);
-        let _io2 = gpioi.pi9.into_af5(&mut gpioi.moder, &mut gpioi.afrh).set_speed(Speed::VeryHigh);
-        let _io3 = gpioh.ph8.into_af5(&mut gpioh.moder, &mut gpioh.afrh).set_speed(Speed::VeryHigh);
-        let _io4 = gpioh.ph9.into_af5(&mut gpioh.moder, &mut gpioh.afrh).set_speed(Speed::VeryHigh);
-        let _io5 = gpioh.ph10.into_af5(&mut gpioh.moder, &mut gpioh.afrh).set_speed(Speed::VeryHigh);
-        let _io6 = gpiog.pg9.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::VeryHigh);
-        let _io7 = gpiog.pg10.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::VeryHigh);
-
-
-
+        let _io0 = gpioi.pi11.into_af5(&mut gpioi.moder, &mut gpioi.afrh).set_speed(Speed::High);
+        let _io1 = gpioi.pi10.into_af5(&mut gpioi.moder, &mut gpioi.afrh).set_speed(Speed::High);
+        let _io2 = gpioi.pi9.into_af5(&mut gpioi.moder, &mut gpioi.afrh).set_speed(Speed::High);
+        let _io3 = gpioh.ph8.into_af5(&mut gpioh.moder, &mut gpioh.afrh).set_speed(Speed::High);
+        let _io4 = gpioh.ph9.into_af5(&mut gpioh.moder, &mut gpioh.afrh).set_speed(Speed::High);
+        let _io5 = gpioh.ph10.into_af5(&mut gpioh.moder, &mut gpioh.afrh).set_speed(Speed::High);
+        let _io6 = gpiog.pg9.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::High);
+        let _io7 = gpiog.pg10.into_af5(&mut gpiog.moder, &mut gpiog.afrh).set_speed(Speed::High);
 
         // Очищение регистров
         ospi.fcr.write(|w| {
@@ -78,10 +79,8 @@ impl OSpi {
         ospi.cr.modify(|_, w| unsafe {
             w.en()
                 .set_bit()
-                .apms()
-                .set_bit()
                 .fthres()
-                .bits(3u8 - 1u8)
+                .bits(4u8 - 1u8)
         });
         while ospi.sr.read().busy().bit_is_set() {}
 
@@ -94,100 +93,152 @@ impl OSpi {
         )};
         while ospi.sr.read().busy().bit_is_set() {}
         // Заполнение device configuration register 2
-        unsafe { ospi.dcr2.modify(|_, w| w.prescaler().bits(2u8)) };
+        unsafe { ospi.dcr2.modify(|_, w| w.prescaler().bits(1u8)) };
+        while ospi.sr.read().busy().bit_is_set() {}
+
+        // // Заполнение data length register
+        // ospi.dlr.write(|w| unsafe { w.dl().bits(3u32 - 1) });
+        // while ospi.sr.read().busy().bit_is_set() {}
+
+        // Заполнение communication configuration register параметрами команды
+        ospi.ccr.modify(|_, w| unsafe {
+            w.admode()
+                .bits(0)
+                .adsize()
+                .bits(0)
+                .abmode()
+                .bits(0)
+                .absize()
+                .bits(0)
+                .dmode()
+                .bits(0u8)
+                .imode()
+                .bits(1u8)
+                .ddtr()
+                .clear_bit()
+                .isize()
+                .bits(0u8)
+        });
+        while ospi.sr.read().busy().bit_is_set() {}
+
+        // Заполнение timing configuration register
+        ospi.tcr.modify(|_, w| unsafe { w.dcyc().bits(0) });
+        while ospi.sr.read().busy().bit_is_set() {}
+
+        // Заполнение instruction register кодом команды
+        ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0x06) });
+        while ospi.sr.read().busy().bit_is_set() {}
+
+        // // Указание режима работы ostospi, обязательно последним
+        // ospi.cr.modify(|_, w| unsafe {w.fmode().bits(0u8)});
+        // while ospi.sr.read().busy().bit_is_set() {}
+        info!("1");
+        // Заполнение регистра адреса, чтобы стриггерить отправку команды
+        //ospi.ar.modify(|_, w| unsafe { w.address().bits(0x0) });
+        //ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0x06) });
+
+        if ospi.sr.read().tef().bit_is_set() {
+            println!("Transfer error");
+        }
+        while ospi.sr.read().busy().bit_is_set() {}
+
+        ospi.fcr.write(|w| {w.ctcf().set_bit()});
+        while ospi.sr.read().busy().bit_is_set() {}
+
+        // Заполнение communication configuration register параметрами команды
+        ospi.ccr.modify(|_, w| unsafe {
+            w.admode()
+                .bits(0)
+                .adsize()
+                .bits(0)
+                .abmode()
+                .bits(0)
+                .absize()
+                .bits(0)
+                .dmode()
+                .bits(1u8)
+                .imode()
+                .bits(1u8)
+                .ddtr()
+                .clear_bit()
+                .isize()
+                .bits(0u8)
+        });
         while ospi.sr.read().busy().bit_is_set() {}
 
         // Заполнение data length register
-        ospi.dlr.write(|w| unsafe { w.dl().bits(3u32 - 1) });
-        while ospi.sr.read().busy().bit_is_set() {}
-
-        // Заполнение communication configuration register параметрами команды
-        ospi.ccr.modify(|_, w| unsafe {
-            w.admode()
-                .bits(0)
-                .adsize()
-                .bits(0)
-                .abmode()
-                .bits(0)
-                .absize()
-                .bits(0)
-                .dmode()
-                .bits(1u8)
-                .imode()
-                .bits(1u8)
-                .ddtr()
-                .clear_bit()
-                .isize()
-                .bits(0u8)
-        });
+        ospi.dlr.write(|w| unsafe { w.dl().bits(1u32 - 1) });
         while ospi.sr.read().busy().bit_is_set() {}
 
         // Заполнение timing configuration register
-        ospi.tcr.modify(|_, w| unsafe { w.dcyc().bits(0b0) });
+        ospi.tcr.modify(|_, w| unsafe { w.dcyc().bits(0) });
         while ospi.sr.read().busy().bit_is_set() {}
 
         // Заполнение instruction register кодом команды
-        ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0x06F9) });
+        ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0x05) });
         while ospi.sr.read().busy().bit_is_set() {}
 
-        // Указание режима работы ostospi, обязательно последним
-        ospi.cr.modify(|_, w| unsafe {w.fmode().bits(0u8)});
+        // Указание режима работы octospi, обязательно последним
+        ospi.cr.modify(|_, w| unsafe {w.fmode().bits(1u8)});
         while ospi.sr.read().busy().bit_is_set() {}
-        info!("{:x?}", &ospi.dr as *const _ as *const u8);
-        // Заполнение регистра адреса, чтобы стриггерить отправку команды
+
         //ospi.ar.modify(|_, w| unsafe { w.address().bits(0x0) });
-        ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0x06F9) });
+        ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0x05) });
 
-        if ospi.sr.read().tef().bit_is_set() {
-            println!("Transfer error");
-        }
+        // if ospi.sr.read().tef().bit_is_set() {
+        //     println!("Transfer error");
+        // }
+        info!("2");
 
-        // Заполнение communication configuration register параметрами команды
-        ospi.ccr.modify(|_, w| unsafe {
-            w.admode()
-                .bits(1)
-                .adsize()
-                .bits(3)
-                .abmode()
-                .bits(0)
-                .absize()
-                .bits(0)
-                .dmode()
-                .bits(0u8)
-                .imode()
-                .bits(1u8)
-                .ddtr()
-                .clear_bit()
-                .isize()
-                .bits(1u8)
-        });
-        while ospi.sr.read().busy().bit_is_set() {}
+        // // Заполнение communication configuration register параметрами команды
+        // ospi.ccr.modify(|_, w| unsafe {
+        //     w.admode()
+        //         .bits(1)
+        //         .adsize()
+        //         .bits(3)
+        //         .abmode()
+        //         .bits(0)
+        //         .absize()
+        //         .bits(0)
+        //         .dmode()
+        //         .bits(0u8)
+        //         .imode()
+        //         .bits(1u8)
+        //         .ddtr()
+        //         .clear_bit()
+        //         .isize()
+        //         .bits(1u8)
+        // });
+        // while ospi.sr.read().busy().bit_is_set() {}
 
-        // Заполнение timing configuration register
-        ospi.tcr.modify(|_, w| unsafe { w.dcyc().bits(0b0) });
-        while ospi.sr.read().busy().bit_is_set() {}
+        // // Заполнение timing configuration register
+        // ospi.tcr.modify(|_, w| unsafe { w.dcyc().bits(0b0) });
+        // while ospi.sr.read().busy().bit_is_set() {}
 
-        // Заполнение instruction register кодом команды
-        ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0xDC0) });
-        while ospi.sr.read().busy().bit_is_set() {}
+        // // Заполнение instruction register кодом команды
+        // ospi.ir.modify(|_, w| unsafe { w.instruction().bits(0xEC13) });
+        // while ospi.sr.read().busy().bit_is_set() {}
 
-        // Указание режима работы ostospi, обязательно последним
-        ospi.cr.modify(|_, w| unsafe {w.fmode().bits(0u8)});
-        while ospi.sr.read().busy().bit_is_set() {}
+        // // Указание режима работы ostospi, обязательно последним
+        // ospi.cr.modify(|_, w| unsafe {w.fmode().bits(1u8)});
+        // while ospi.sr.read().busy().bit_is_set() {}
 
-        ospi.ar.modify(|_, w| unsafe { w.address().bits(0x0) });
+        // ospi.ar.modify(|_, w| unsafe { w.address().bits(0x0) });
 
-        if ospi.sr.read().tef().bit_is_set() {
-            println!("Transfer error");
-        }
+        // if ospi.sr.read().tef().bit_is_set() {
+        //     println!("Transfer error");
+        // }
 
-        // let buffer: &mut [u8] = &mut [0u8; 3];
-        // let mut b = buffer.iter_mut();
+        let buffer: &mut [u8] = &mut [0u8; 3];
+        let mut b = buffer.iter_mut();
         // while ospi.sr.read().tcf().bit_is_clear() {
         //     if ospi.sr.read().ftf().bit_is_set() {
         //         if let Some(v) = b.next() {
         //             unsafe {
-        //                 *v = ptr::read_volatile(&ospi.dr as *const _ as *const u8);
+        //                 info!("3");
+                        buffer[0] = unsafe {ptr::read_volatile(&ospi.dr as *const _ as *const u8)};
+                        buffer[1] = unsafe {ptr::read_volatile(&ospi.dr as *const _ as *const u8)};
+                        buffer[2] = unsafe {ptr::read_volatile(&ospi.dr as *const _ as *const u8)};
         //             }
         //         } else {
         //             println!("Error");
@@ -195,7 +246,7 @@ impl OSpi {
         //         }
         //     }
         // }
-        // // When transfer complete, empty fifo buffer
+        // When transfer complete, empty fifo buffer
         // while ospi.sr.read().flevel().bits() > 0 {
         //     if let Some(v) = b.next() {
         //         unsafe {
@@ -206,6 +257,7 @@ impl OSpi {
         //     }
         // }
 
-        println!("Finally!! Congratulations");
+        println!("Finally!! Congratulations: {:x?}", &buffer[..]);
+
     }
 }
